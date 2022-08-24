ef4fa17675433a1b5af7a506969d326e45161fdb
LeeInSeuk <inseuk1007@naver.com>
2022-08-25 00:27:30 +0900

    C도 알고리즘 시작!

---
+++ C언어/beakjoon/백준.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+/* //백준 1000번
+int main() {
+	int A, B;
+
+	scanf("%d %d", &A, &B);
+
+	printf("%d", A + B);
+
+	return 0;
+
+}*/
+
+/*   //백준 2558번
+int main() {
+	int A = 0, B = 0;
+
+	scanf("%d",&A);
+	scanf("%d",&B);
+
+	printf("%d",A + B);
+
+	return 0;
+}
+*/
+
+int main() {
+
+	int A = 0, B = 0;
+
+	while (scanf("%d %d", &A, &B); )
+}
